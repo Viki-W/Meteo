611a4997648e1eb23fdb5b8c18341382038a77a1
Viki W <oysters109@163.com>
2013-12-18 23:41:09 +0800

    v0.1 Initial

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿var express = require('express'),
+    app = express(),
+    port = 8000;
+	
+app.configure(function(){
+    app.use(app.router);
+    app.use("/css", express.static(__dirname + '/css'));
+    app.use("/lib", express.static(__dirname+ '/lib'));
+	app.use("/js", express.static(__dirname+ '/js'));
+});
+
+app.get('/todo',function(req, res){
+    res.sendfile(__dirname + '/index.html');
+});
+
+app.listen(port);
+console.log("Node listening on port " + port);
---
+++ js/todo.js
@@ -0,0 +1,47 @@
+﻿angular.module('todo', ['ngCookies']).
+controller('TodoCtrl', ['$scope', '$http', '$filter', '$cookieStore',
+    function TodoCtrl($scope, $http, $filter, $cookieStore) {
+        $scope.today = $filter('date')(new Date(),'MM/dd/yyyy');
+        $scope.dataUrl = $scope.today.replace(/\//g,'') + '.json';
+        
+        $scope.todos = angular.fromJson($cookieStore.get('allTodos'));
+        //console.log($scope.todos);
+        if (!$scope.todos){
+            $scope.todos = [{
+                    summary: ((function () {
+                        var today = new Date();
+                        var firstDay = new Date("2013/11/12");
+                        var days = parseInt((today.getTime() - firstDay.getTime()) / (1000 * 60 * 60 * 24), 0);
+                        return 'Vocabulary Unit ' + days;
+                        }))(),
+                    done: false
+                }];
+        }            
+                    
+        $scope.addTodo = function () {
+            if($scope.todoSummary) {
+                var newTodo = {
+                    'summary': $scope.todoSummary, 
+                    'done': false 
+                };
+                $scope.todos.push(newTodo);
+                $scope.todoSummary = '';
+                $scope.saveCookie();
+            }
+        };
+
+        $scope.archive = function () {
+            var oldTodos = $scope.todos;
+            $scope.todos = [];
+            angular.forEach(oldTodos, function (todo) {
+                if (!todo.done) {
+                    $scope.todos.push(todo);
+                }
+            });
+            $scope.saveCookie();
+        };
+        
+        $scope.saveCookie = function(){
+            $cookieStore.put('allTodos',angular.toJson($scope.todos));
+        };    
+}]);
