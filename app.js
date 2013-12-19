98876f4ed9e784886b6317d500464093361b62be
Viki W <oysters109@163.com>
2013-12-20 01:07:10 +0800

    Add Node

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-﻿var express = require('express'),
+﻿var fs = require('fs'),
+	express = require('express'),
     app = express(),
-    port = 8000;
+	port = 8080;
 	
 app.configure(function(){
     app.use(app.router);
@@ -13,5 +14,34 @@ app.get('/todo',function(req, res){
     res.sendfile(__dirname + '/index.html');
 });
 
+app.get('/todo/api',function(req, res){
+	var filepath = __dirname + '/data.json';
+	fs.readFile(filepath, 'utf-8',function (err, data) {
+			if (err) {
+				res.send(500);
+			}
+			res.send(data);
+	});    
+});
+
+app.post('/todo/api',function(req, res){
+	var body = "";
+	req.on('data', function (chunk) {
+		body += chunk;
+	});
+	req.on('end', function () {
+		var filepath = __dirname + '/data.json';	
+		fs.writeFile(filepath,body,function(err){
+			if (err) {
+				res.send(500);
+			}
+		});
+		res.send(body);
+	});
+});
+
 app.listen(port);
 console.log("Node listening on port " + port);
+
+
+
--- js/todo.js
+++ js/todo.js
@@ -1,33 +1,46 @@
-﻿angular.module('todo', ['ngCookies']).
-controller('TodoCtrl', ['$scope', '$http', '$filter', '$cookieStore',
-    function TodoCtrl($scope, $http, $filter, $cookieStore) {
+﻿angular.module('todo', []).
+controller('TodoCtrl', ['$scope', '$http', '$filter', 
+    function TodoCtrl($scope, $http, $filter) {
         $scope.today = $filter('date')(new Date(),'MM/dd/yyyy');
-        $scope.dataUrl = $scope.today.replace(/\//g,'') + '.json';
-        
-        $scope.todos = angular.fromJson($cookieStore.get('allTodos'));
-        //console.log($scope.todos);
-        if (!$scope.todos){
-            $scope.todos = [{
-                    summary: ((function () {
-                        var today = new Date();
-                        var firstDay = new Date("2013/11/12");
-                        var days = parseInt((today.getTime() - firstDay.getTime()) / (1000 * 60 * 60 * 24), 0);
-                        return 'Vocabulary Unit ' + days;
-                        }))(),
-                    done: false
-                }];
-        }            
-                    
+        $scope.dataUrl = $scope.today.replace(/\//g,'') + '.json';	
+		
+		$http.get('/todo/api').
+		    success(function(data){
+				$scope.todos = data;
+			}).
+			error(function(){
+				$scope.todos = [{
+					summary: ((function () {
+						var today = new Date();
+						var firstDay = new Date("2013/11/12");
+						var days = parseInt((today.getTime() - firstDay.getTime()) / (1000 * 60 * 60 * 24), 0) + 1;
+							return 'Vocabulary Unit ' + days;
+						}))(),
+					done: false
+				}];
+			});	
+		
+		$scope.saveData = function(){
+			$http.post('/todo/api', JSON.stringify($scope.todos)).
+				success(function(){
+					console.log('Data Saved ');
+				}).
+			error(function(){
+				console.log('Failed to save data ');
+			});
+        }; 
+                                   
         $scope.addTodo = function () {
             if($scope.todoSummary) {
                 var newTodo = {
-                    'summary': $scope.todoSummary, 
+                    'summary': $scope.todoSummary,
                     'done': false 
                 };
                 $scope.todos.push(newTodo);
-                $scope.todoSummary = '';
-                $scope.saveCookie();
-            }
+				$scope.todoSummary = '';
+				$scope.saveData();
+				console.log('55' + $scope.todos );
+            }		
         };
 
         $scope.archive = function () {
@@ -36,12 +49,11 @@ controller('TodoCtrl', ['$scope', '$http', '$filter', '$cookieStore',
             angular.forEach(oldTodos, function (todo) {
                 if (!todo.done) {
                     $scope.todos.push(todo);
+					$scope.saveData();
                 }
             });
-            $scope.saveCookie();
         };
         
-        $scope.saveCookie = function(){
-            $cookieStore.put('allTodos',angular.toJson($scope.todos));
-        };    
+		
+   
 }]);
